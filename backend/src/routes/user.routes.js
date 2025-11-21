6d2124a1aafec1bb8be83939eda8e8ed11d20f46
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-11-21 17:23:28 +0530

    fix: controller route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,24 +1,22 @@
 import express from "express";
 import { registerUser } from "../controllers/register.controller.js";
 import loginUser from "../controllers/login.controller.js";
-import { 
-  expenseController,
-  getAllExpence,
-  deleteExpence,
-  updateExpense 
-} from "../controllers/expence.controller.js";
-import protect from "../middlewares/user.middlewares.js";
+import {expenseController,getAllExpence,deleteExpence,updateExpense} from "../controllers/expence.controller.js";
+import protect from "../middlewares/user.middlewares.js"
 
 const router = express.Router();
 
-// Auth Routes
+// POST /api/auth/register
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-// Expense Routes
-router.post("/expense", protect, expenseController);       // Add expense
-router.get("/expenses", protect, getAllExpence);            // Get all expenses
-router.delete("/expense/:id", protect, deleteExpence);      // Delete expense
-router.put("/expense/:id", protect, updateExpense);         // Update expense
+
+router.post("/expence",protect , expenseController);
+router.get("/getAllExpence",protect , getAllExpence);
+router.post("/deleteExpence/:id",protect ,deleteExpence);
+
+
+
+
 
 export default router;
